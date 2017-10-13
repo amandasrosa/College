38c616f08b58a4d89f977821cff0a150fde93801
amandasrosa <amandasrosa@hotmail.com>
2017-10-13 15:30:41 -0400

    Add files via upload

---
+++ College/College/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  College
+//
+//  Created by MacStudent on 2017-10-06.
+//  Copyright © 2017 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+var reports = CollegeReports()
+
+print("Instructors by classes and average student grades")
+print(reports.instructorsByClasses())
+print("")
+
+print("Number of students by country")
+print(reports.studentsByCountry())
+print("")
+
+print("Top 3 courses with worst average grades")
+print(reports.coursesByWorstAverage())
+print("")
+
+print("Number of classes per instructor per weekday")
+print(reports.classesByInscructorsPerWeek())
+print("")
+
+print("Top student with best average grade by program")
+print(reports.studentsByBestAverage())
+print("")
